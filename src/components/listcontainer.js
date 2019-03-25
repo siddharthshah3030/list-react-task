c76a79410da223939a1acb67d892416ed4374d24
siddharth shah <siddharthshah3030@gmail.com>
2019-03-25 18:10:09 +0530

    adding list container

--- src/components/listcontainer.js
+++ src/components/listcontainer.js
@@ -1,26 +1,48 @@
 import React from "react";
+import "../styles/loader.scss";
 
-class Box extends React.Component {
+class listContainer extends React.Component {
   state = {
-    loading: true
+    loading: true,
+    list: []
   };
 
   componentDidMount() {
-    fetchList().then(() => this.setState({ loading: false }));
+    fetchList
+      .then(list => {
+        console.log(list[2].name);
+        this.setState({ list });
+      })
+      .then(() => this.setState({ loading: false }));
   }
 
   render() {
     const { loading } = this.state;
 
     if (loading) {
-      return <div> I am the loader</div>;
+      return <div className="loader"> I am the loader</div>;
     }
 
-    return <div>I'm the app</div>;
+    return (
+      <div>
+        I'm the app <br />
+        {this.state.list[3].name}
+      </div>
+    );
   }
 }
 
-function fetchList() {
-  return new Promise(resolve => setTimeout(() => resolve(), 3000));
-}
-export default Box;
+var fetchList = new Promise(function(resolve, reject) {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      resolve(data);
+    })
+    .catch(err => {
+      // Do something for an error here
+    });
+});
+
+export default listContainer;
