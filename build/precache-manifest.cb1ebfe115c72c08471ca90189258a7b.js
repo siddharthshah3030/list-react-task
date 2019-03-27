df1d5a384a0ea6afb4e72375f30de5ff2559819e
siddharth shah <siddharthshah3030@gmail.com>
2019-03-28 03:40:43 +0530

    removing build from .gitignore

---
+++ build/precache-manifest.cb1ebfe115c72c08471ca90189258a7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a6d0c839be64cbe3e78",
+    "url": "/static/css/main.7b15d91c.chunk.css"
+  },
+  {
+    "revision": "1a6d0c839be64cbe3e78",
+    "url": "/static/js/main.c85f7969.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "5fc4651b0b731b2d7a3a",
+    "url": "/static/js/2.d6ef2abc.chunk.js"
+  },
+  {
+    "revision": "a6a6686cbddb3e99a5f0b60a829effb3",
+    "url": "/static/media/loader.a6a6686c.gif"
+  },
+  {
+    "revision": "47c7a3bf22fca56158513e321fcb8b57",
+    "url": "/index.html"
+  }
+];
